49b67e1984ce16ad8b32f969336a6fc00853e05f
Jón Gabríel Lorange <jonl17@ru.is>
2021-12-05 12:00:07 +0000

    seo

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -15,6 +15,16 @@ const MainLayout: React.FC = ({ children }) => {
           href='https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap'
           rel='stylesheet'
         />
+        <link rel='icon' type='image/png' href='/favicon.png' />
+        <title>TSS | earth citizen</title>
+        <meta
+          property='description'
+          content='An artist from Iceland. Music, drawings, programming and more?'
+        />
+        <meta
+          property='og:description'
+          content='An artist from Iceland. Music, drawings, programming and more?'
+        />
       </Helmet>
       {children}
     </main>
