b815f12cea458bdf07b1277dc90a349cdadb6b6b
Jón Gabríel Lorange <jonl17@ru.is>
2021-12-05 10:59:48 +0000

    centering in shit browsers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,8 +16,10 @@ export default function Home({ data: { imageSharp } }: Props) {
     <div className='h-screen w-full bg-gray-50'>
       <div className='grid place-items-center h-full w-full'>
         <h1 className='text-8xl text-gray-800'>TSS</h1>
-        <div className='absolute'>
-          <Circle text='The Suburban Spaceman' />
+        <div className='absolute h-screen w-screen grid place-items-center'>
+          <div>
+            <Circle text='The Suburban Spaceman' />
+          </div>
         </div>
       </div>
     </div>
