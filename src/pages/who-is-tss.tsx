61bc0d32dd94c423d7a4262d55df168bdf5b89c0
Jón Gabríel Lorange <jonl17@ru.is>
2021-12-05 12:46:37 +0000

    scale

--- src/pages/who-is-tss.tsx
+++ src/pages/who-is-tss.tsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <Link to='/'>
       <div className='min-h-screen h-full w-screen bg-yellow-200  grid place-items-center'>
-        <p className='text-green-400 text-8xl p-5 max-w-lg'>
+        <p className='text-green-400 text-7xl desktop:text-8xl p-5 max-w-lg'>
           TSS: An artist from Iceland. Music, drawings,
           internet art and more?
         </p>
